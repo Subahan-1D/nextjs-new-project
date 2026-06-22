a26d735311c6a1a891b387c4f28c52a4e01e2bb5
Subahan Ali <subahanislam523@gmail.com>
2026-06-22 22:49:43 +0600

    added to by abbout page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const aboutPage = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>This is the about page of our Next.js application.</p>
+        </div>
+    );
+};
+export default aboutPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import React from 'react';
 
-export default function Home() {
+const homePage = () => {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="text-3xl font-bold">Welcome to Next.js!</h1>
-      </main>
+    <div>
+      <h1>Welcome to the Home Page</h1>
     </div>
   );
-}
+};
+
+export default homePage;
