44f2b067c39568a5328c6a64cbef15b6895f3fa6
Subahan Ali <subahanislam523@gmail.com>
2026-06-26 16:53:07 +0600

    added to image optimaization and font change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 
+import styles from './About.module.css';
 const AboutPage = () => {
     return (
         <div>
             <h1 className="text-2xl font-bold">About Us</h1>
-            <p>This is the about page of our Next.js application.</p>
+            <p className={styles.text_style}>This is the about page of our Next.js application.</p>
         </div>
     );
 };
