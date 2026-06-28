8aca12d589ec389b54052081a95b359a30fa44cb
Subahan Ali <subahanislam523@gmail.com>
2026-06-28 21:02:20 +0600

    added to by a my product card

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+'use client";'
 
-const Products = () => {
-    return (
-        <div className="min-h-screen flex items-center justify-center  px-6">
-            <h1>
-                Products Page
-            </h1>
-        </div>
-    );
+import ProductCard from "@/src/components/productCard";
+
+
+const Products = async () => {
+  const res = await fetch("http://localhost:5000/products", {
+    cache: "no-store",
+  });
+
+  const products = await res.json();
+
+  return (
+    <div className="max-w-7xl mx-auto py-10 px-5">
+      <h1 className="text-4xl font-bold mb-8 text-center">
+        Our Products
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {products.map((product: any) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Products;
---
+++ src/components/productCard.tsx
@@ -0,0 +1,47 @@
+'use client';
+import Image from "next/image";
+
+type Product = {
+  id: number;
+  product_name: string;
+  category: string;
+  brand: string;
+  price: number;
+  image: string;
+};
+
+const ProductCard = ({ product }: { product: Product }) => {
+  return (
+    <div className="border rounded-xl shadow-md overflow-hidden hover:shadow-xl transition duration-300">
+      <Image
+        src={product.image}
+        alt={product.product_name}
+        width={200}
+        height={200}
+        className="w-full h-36 object-cover"
+      />
+
+      <div className="p-2">
+        <h2 className="text-xl font-bold">{product.product_name}</h2>
+
+        <p className="text-gray-500 mt-2">
+          Brand: {product.brand}
+        </p>
+
+        <p className="text-gray-500">
+          Category: {product.category}
+        </p>
+
+        <p className="text-2xl font-bold text-green-600 mt-3">
+          ${product.price}
+        </p>
+
+        <button className="w-full mt-4 bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
